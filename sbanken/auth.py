45214947a319fcf59e2d61b7757d6ecfb0b15a09
Rolf K <abstractstar@gmail.com>
2020-02-07 21:14:14 +0100

    Remove commented out code

--- sbanken/auth.py
+++ sbanken/auth.py
@@ -41,14 +41,6 @@ class Auth:
 
         return self.token["access_token"]
 
-        # print(token)
-
-        # async with self.websession.get(
-        #    f"{AUTH_ROOT}/identityserver/connect/token",
-        #    json={"client_id": self.client_id, "secret": self.client_secret},
-        # ) as r:
-        #    print(r.status)
-
     async def request(self, method, url, **kwargs) -> ClientResponse:
         """ Make a request """
         headers = kwargs.get("headers")
@@ -64,60 +56,3 @@ class Auth:
         return await self.websession.request(
             method, f"{API_ROOT}/{url}", **kwargs, headers=headers
         )
-
-
-# class Auth:
-#    def __init__(
-#        self,
-#        token: Optional[Dict[str, str]] = None,
-#        customer_id: str = None,
-#        client_id: str = None,
-#        client_secret: str = None,
-#        token_updater: Optional[Callable[[str], None]] = None,
-#    ):
-#
-#        self.auth_host = AUTH_ROOT
-#        self.api_host = API_ROOT
-#        self.customer_id = customer_id
-#        self.client_id = client_id
-#        self.client_secret = client_secret
-#        self.token_updater = token_updater
-#
-#        extra = {"client_id": self.client_id, "secret": self.client_secret}
-#
-#        oauth2_client = BackendApplicationClient(client_id=client_id)
-#        self._oauth = OAuth2Session(
-#            client=oauth2_client,
-#            auto_refresh_kwargs=extra,
-#            client_id=client_id,
-#            token=token,
-#            token_updater=token_updater,
-#        )
-#
-#    def refresh_tokens(self) -> Dict[str, Union[str, int]]:
-#        """ Refresh and return new tokens """
-#        token = self._oauth.fetch_token(
-#            token_url=f"{self.auth_host}/identityserver/connect/token",
-#            client_id=self.client_id,
-#            client_secret=self.client_secret,
-#        )
-#
-#        if self.token_updater is not None:
-#            self.token_updater(token)
-#
-#        return token
-#
-#    def request(self, method: str, path: str, **kwargs) -> Response:
-#        """ Make a request """
-#        url = f"{self.api_host}/{path}"
-#        try:
-#            return getattr(self._oauth, method)(
-#                url, headers={"customerId": self.customer_id}, **kwargs
-#            )
-#        except TokenExpiredError:
-#            self._oauth.token = self.refresh_tokens()
-#            return getattr(self._oauth, method)(
-#                url, headers={"customerId": self.customer_id}, **kwargs
-#            )
-#
-#
