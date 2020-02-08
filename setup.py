7114f074ee1b16a37757bbf5baf6255514f5c4c9
Rolf K <abstractstar@gmail.com>
2020-02-08 12:38:08 +0100

    v0.4

--- setup.py
+++ setup.py
@@ -4,24 +4,16 @@ from setuptools import setup
 
 setup(
     name="sbanken-api",
-    version="0.3",
-    description="Easily communicate with Sbanken API in python",
+    version="0.4",
+    description="Easy (async) communication with Sbanken API in python",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
     author="Rolf Klausen",
     author_email="redacted@mail.com",
     url="https://github.com/abstrakct/sbanken-python",
-    download_url="https://github.com/abstrakct/sbanken-python/archive/v0.3.tar.gz",
+    download_url="https://github.com/abstrakct/sbanken-python/archive/v0.4.tar.gz",
     packages=["sbanken"],
-    install_requires=[
-        "certifi",
-        "chardet",
-        "idna",
-        "oauthlib",
-        "requests",
-        "requests-oauthlib",
-        "urllib3",
-    ],
+    install_requires=["oauthlib", "requests", "requests-oauthlib",],
     classifiers=[  # https://pypi.python.org/pypi?%3Aaction=list_classifiers
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
